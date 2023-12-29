18dfba4bf5334987b7b8a3de46aafb6fe93d19ac
kunrak <rakeshkunducsgi@gmail.com>
2023-12-29 13:30:52 +0530

    app.test.js file

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("click event test case", () => {
+  render(<App />);
+  const btn = screen.getByRole("button");
+  fireEvent.click(btn);
+  expect(screen.getByText("Updated Data")).toBeInTheDocument();
+});
