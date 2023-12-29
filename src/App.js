bf5217c887426a82df784ed7637cf53b16f3a72d
kunrak <rakeshkunducsgi@gmail.com>
2023-12-29 13:22:16 +0530

    event-change-tut

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
   return (
     <div className="App">
-      <p>First React Test Case</p>
-      <p>Rakesh Kundu</p>
-      <input type="text" placeholder="Enter user name" name="username" id="userId" value="Rakesh Kundu" />
-      <br />
-      <img
-        title="AI generated img"
-        src="https://img.freepik.com/free-photo/medium-shot-woman-training-outdoors_23-2150405486.jpg?w=740&t=st=1703598706~exp=1703599306~hmac=f26062c470676e3b1bd66d274a140d2f0b0612ef09f52963f769565f215acd99"
-        height={600}
-      />
+      <h1>Test Click Event with Button</h1>
+      <button onClick={() => setData("Updated Data")}>Update Data</button>
+      <h1>{data}</h1>
     </div>
   );
 }
