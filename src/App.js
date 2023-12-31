ed3bf5d569939ca2782b7ec7299a48bc8fb27993
kunrak <rakeshkunducsgi@gmail.com>
2023-12-31 23:51:49 +0530

    click-event-test-tut

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import handleOtherMethod from "./helper";
 
 function App() {
   const [data, setData] = useState("");
+
+  const handleData = () => {
+    setData("Hello");
+  };
+
   return (
     <div className="App">
-      <h1>Test OnChange Event with Text Input</h1>
+      <h1>Functional Method Component Testing</h1>
+      <button data-testid="btn1" onClick={handleData}>
+        Update
+      </button>
+      <button onClick={handleOtherMethod}>Print</button>
+      <h2>{data}</h2>
     </div>
   );
 }
